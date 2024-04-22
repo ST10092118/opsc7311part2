82a2a00fb64ed8c68479eb9bbfedfdd4c7d87eee
Semeshan Thaver <ST10092118@vcconnect.edu.za>
2024-04-22 14:01:03 +0200

    added login menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.material:material:1.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/opsc7311/opsc7311poepart2/MainActivity.kt
+++ app/src/main/java/com/opsc7311/opsc7311poepart2/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.opsc7311.opsc7311poepart2.fragments.EntriesFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+
+        val EntriesFragment = EntriesFragment()
+
+        makeCurrentFragment(EntriesFragment)
+
+        bottomNavigation.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.entries_mi -> makeCurrentFragment(EntriesFragment)
+            }
+            true
+
         }
+
     }
-}
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
+
+
+    }
+
